c46c832688eb12a7d1028e8ec722532bfff68537
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-03-23 13:05:11 -0400

    Initial commit

---
+++ ExceptionHandlerAnnotations/ExceptionHandlerAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExceptionHandlerAnnotations;
+
+/// <summary>
+/// An attribute used to designate a class as an exception handler.
+/// </summary>
+/// <remarks>
+/// This attribute is intended to be applied to classes that are specifically designed
+/// to handle exceptions in a centralized or context-specific manner. By applying this
+/// attribute, the annotated class can be recognized as an exception handler in a
+/// larger framework or system architecture.
+/// </remarks>
+/// <seealso cref="GeneralExceptionHandlerAttribute"/>
+[AttributeUsage(AttributeTargets.Class)]
+public class ExceptionHandlerAttribute : Attribute;
---
+++ ExceptionHandlerGenerator/ExceptionHandlerGenerator/AnalyzerReleases.Unshipped.md
@@ -0,0 +1,5 @@
+### New Rules
+| Rule ID | Category | Severity | Notes                             |
+|---------|----------|----------|-----------------------------------|
+| EHG001  | Design | Warning  | EHG001_ExceptionHandlerGenerator |
+| EHG002 | Design   | Error    | EHG001_ExceptionHandlerGenerator              |
